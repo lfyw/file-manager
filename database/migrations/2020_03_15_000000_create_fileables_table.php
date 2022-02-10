017e5a56fc3508bca0f35f98598ada02095bb15c
lfyw <1559897062@qq.com>
2022-02-10 10:07:06 +0800

    fix: migrations

--- database/migrations/2020_03_15_000000_create_fileables_table.php
+++ database/migrations/2020_03_15_000000_create_fileables_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFileableTable extends Migration
+class CreateFileablesTable extends Migration
 {
     /**
      * Run the migrations.
